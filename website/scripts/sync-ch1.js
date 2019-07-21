ff510f5c39bee892c69d1ffcb9772531ff50ab7d
burrrata <42473928+burrrata@users.noreply.github.com>
2019-07-21 18:11:20 -0400

    created local version of ch1.1 coding challenge

--- website/scripts/sync-ch1.js
+++ website/scripts/sync-ch1.js
@@ -6,11 +6,13 @@ const REPO = 'cryptoeconomics-study/code/'
 // note: for the sync scripts to work the contentLocation fields are CASE SENSITIVE
 
 const pages = [
+	/* temporarily building this locally to test changes
 	{
 		destination: 'docs/sync/ch1.1-code-challenge.md',
 		title: 'Code Challenge',
 		contentLocation: 'c1_CentralPaymentOperator/1.1-Hashes_and_Signatures/README.md'
 	},
+	*/
 	{
 		destination: 'docs/sync/ch1.2-code-challenge.md',
 		title: 'Code Challenge',
