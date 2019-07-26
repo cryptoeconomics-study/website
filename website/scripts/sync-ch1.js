26698020b9b3b24a65e6a56e0e0af9df37cfa30d
burrrata <42473928+burrrata@users.noreply.github.com>
2019-07-25 21:43:57 -0400

    reset ch1 sync

--- website/scripts/sync-ch1.js
+++ website/scripts/sync-ch1.js
@@ -6,13 +6,11 @@ const REPO = 'cryptoeconomics-study/code/'
 // note: for the sync scripts to work the contentLocation fields are CASE SENSITIVE
 
 const pages = [
-	/* temporarily building this locally to test changes
 	{
 		destination: 'docs/sync/ch1.1-code-challenge.md',
 		title: 'Code Challenge',
-		contentLocation: 'c1_CentralPaymentOperator/1.1-Hashes_and_Signatures/README.md'
+		contentLocation: 'c1_CentralPaymentOperator/1.1%20-%20Hashes_and_Signatures/README.md'
 	},
-	*/
 	{
 		destination: 'docs/sync/ch1.2-code-challenge.md',
 		title: 'Code Challenge',
