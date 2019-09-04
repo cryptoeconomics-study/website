b310ab7bea7c55ce59b49c498c155b716838923e
burrrata <42473928+burrrata@users.noreply.github.com>
2019-09-03 21:48:47 -0400

    _

--- website/scripts/sync-code-repo.js
+++ website/scripts/sync-code-repo.js
@@ -48,60 +48,60 @@ const pages = [
     title: "Code Challenge",
     contentLocation: "ch1/1.5/README.md"
   },
-	// 2.1
-	{
-		destination: 'docs/sync/2.1-code-challenge.md',
-		title: 'Code Challenge',
-		contentLocation: 'ch2/2.1/README.md'
-	},
-	// 2.2
-	{
-		destination: 'docs/sync/2.2-code-challenge.md',
-		title: 'Code Challenge',
-		contentLocation: 'ch2/2.2/README.md'
-	},
-	// 2.3
-	{
-		destination: 'docs/sync/2.3-code-challenge.md',
-		title: 'Code Challenge',
-		contentLocation: 'ch2/2.3/README.md'
-	},
-	// 2.4
-	{
-		destination: 'docs/sync/2.4-code-challenge.md',
-		title: 'Code Challenge',
-		contentLocation: 'ch2/2.4/README.md'
-	},
-	// 3.1
-	{
-		destination: 'docs/sync/3.1-code-challenge.md',
-		title: 'Code Challenge',
-		contentLocation: 'ch3/3.1/README.md'
-	},
-	// 3.2
-	{
-		destination: 'docs/sync/3.2-code-challenge.md',
-		title: 'Code Challenge',
-		contentLocation: 'ch3/3.2/README.md'
-	},
-	// 3.3
-	{
-		destination: 'docs/sync/1.3-code-challenge.md',
-		title: 'Code Challenge',
-		contentLocation: 'ch3/3.3/README.md'
-	},
-	// 3.4
-	{
-		destination: 'docs/sync/1.4-code-challenge.md',
-		title: 'Code Challenge',
-		contentLocation: 'ch3/3.4/README.md'
-	},
-	// 3.5
-	{
-		destination: 'docs/sync/1.5-code-challenge.md',
-		title: 'Code Challenge',
-		contentLocation: 'ch3/3.5/README.md'
-	}
+  // 2.1
+  {
+    destination: "docs/sync/2.1-code-challenge.md",
+    title: "Code Challenge",
+    contentLocation: "ch2/2.1/README.md"
+  },
+  // 2.2
+  {
+    destination: "docs/sync/2.2-code-challenge.md",
+    title: "Code Challenge",
+    contentLocation: "ch2/2.2/README.md"
+  },
+  // 2.3
+  {
+    destination: "docs/sync/2.3-code-challenge.md",
+    title: "Code Challenge",
+    contentLocation: "ch2/2.3/README.md"
+  },
+  // 2.4
+  {
+    destination: "docs/sync/2.4-code-challenge.md",
+    title: "Code Challenge",
+    contentLocation: "ch2/2.4/README.md"
+  },
+  // 3.1
+  {
+    destination: "docs/sync/3.1-code-challenge.md",
+    title: "Code Challenge",
+    contentLocation: "ch3/3.1/README.md"
+  },
+  // 3.2
+  {
+    destination: "docs/sync/3.2-code-challenge.md",
+    title: "Code Challenge",
+    contentLocation: "ch3/3.2/README.md"
+  },
+  // 3.3
+  {
+    destination: "docs/sync/3.3-code-challenge.md",
+    title: "Code Challenge",
+    contentLocation: "ch3/3.3/README.md"
+  },
+  // 3.4
+  {
+    destination: "docs/sync/3.4-code-challenge.md",
+    title: "Code Challenge",
+    contentLocation: "ch3/3.4/README.md"
+  },
+  // 3.5
+  {
+    destination: "docs/sync/3.5-code-challenge.md",
+    title: "Code Challenge",
+    contentLocation: "ch3/3.5/README.md"
+  }
 ];
 
 const locationReferenceMap = {};
